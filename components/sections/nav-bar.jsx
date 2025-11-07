a5f367429adb3d2f4f353b176e12e6278e81b748
AlexanderBraatz <alex.braatz.g@gmail.com>
2025-11-07 12:49:40 +0000

    feat: add header component

--- components/sections/nav-bar.jsx
+++ components/sections/nav-bar.jsx
@@ -1,20 +1,26 @@
+import Image from "next/image";
 import React from "react";
-
+import arrow from "@/public/icons/arrow-right-neon-blue.svg";
+import logo from "@/public/icons/logo-fff.svg";
 export default function NavBar() {
   return (
     <div className="bg-q-blue flex h-[6rem] w-full items-center justify-between border-b-[1px] border-white px-10 text-neutral-50">
-      <div className="h-[62px] w-[203px] bg-green-950">LOGO</div>
-      <div className="flex gap-10">
-        <ul className="font-reem-kufi-fun flex gap-7">
-          <li>Zimmer</li>
+      <Image src={logo} alt="logo" />
+
+      <div className="flex items-center gap-10">
+        <ul className="font-reem-kufi-fun flex gap-7 text-2xl/7 tracking-tighter">
+          <li className="h-7 border-b border-neutral-50">Zimmer</li>
           <li>Ausstattung</li>
           <li>Bewertung</li>
           <li>Anfahrt</li>
         </ul>
         <div>
-          <a>
-            <span className="font-jost">ANFRAGEN</span>
-            <span>icon</span>
+          <a className="pl flex items-center gap-4 rounded-full bg-neutral-50 py-4.5 pr-8 pl-12">
+            <span className="font-jost text-q-neon-blue text text-xl/1 font-bold">
+              ANFRAGEN
+            </span>
+
+            <Image src={arrow} alt="arrow" />
           </a>
         </div>
       </div>
